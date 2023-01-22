255b657ae011e77346736a750259435398ba6ef4
Felipe Geroldi <42786299+felipegeroldi@users.noreply.github.com>
2023-01-22 14:35:57 -0300

    Primeiro programa em Go

--- fundamentos/primeiro/primeiro.go
+++ fundamentos/primeiro/primeiro.go
@@ -11,4 +11,12 @@ import "fmt"
 func main() {
 	fmt.Print("Primeiro ")
 	fmt.Print("Programa!")
+
+	/*
+		Sobre comentários...
+
+		1) Priorize código legível e faça comentários que agrega valor!
+		2) Evite comentários óbvios
+		3) Durante o curso abuse dos comentários
+	*/
 }
